f67bfd5993419375885e30361029ffe7721c611c
Kira <kira.soba@gmx.de>
2023-02-12 19:33:40 +0100

    Initial setup

---
+++ routes/contact.routes.js
@@ -0,0 +1,49 @@
+const router = require("express").Router()
+const nodemailer = require("nodemailer")
+
+// send email via contact form
+router.post("/", async (req, res, next) => {
+  const { sender, email, message } = req.body
+
+  // prepare data
+  const data = {
+    from: email,
+    to: process.env.MAIL_ME,
+    subject: `${sender} via contact form`,
+    text: message,
+    html: `<p><b>Name:</b> ${sender}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Message:</b> ${message}</p>`,
+  }
+
+  // use mailtrap to create transporter
+  // let transporter = nodemailer.createTransport({
+  //     host: "smtp.mailtrap.io",
+  //     port: 2525,
+  //     auth: {
+  //         user: process.env.MAIL_USER,
+  //         pass: process.env.MAIL_PASS
+  //     }
+  // });
+
+  // use mail to create transporter
+  let transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  })
+
+  // send via nodemailer
+  await transporter
+    .sendMail(data)
+    .then((info) => {
+      // console.log('send>> ', info);
+      res.json("Your message has been send to Kira.")
+    })
+    .catch((error) => console.log(error))
+})
+
+module.exports = router
---
+++ routes/project.routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router()
+const projects = require("../db/projects.json")
+const slugify = require("slugify")
+
+// GET all projects
+router.get("/", (req, res, next) => {
+  res.json(projects)
+})
+
+// GET latest project
+router.get("/latest", (req, res, next) => {
+  const latestNo = Math.max(...projects.map((proj) => proj.no))
+  const latestProject = projects.find((el) => el.no === latestNo)
+  res.json(latestProject)
+})
+
+// GET single project
+router.get("/:slug", (req, res, next) => {
+  const { slug } = req.params
+  const project = projects.find((proj) => slugify(proj.name) === slug)
+  res.json(project)
+})
+
+module.exports = router
